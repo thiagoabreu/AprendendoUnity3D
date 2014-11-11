29277a30b9d2eb80d52afb841dfe7f0b8747a3bf
Thiago Jedi <thiagoa7@gmail.com>
2014-11-11 00:03:07 -0300

    Mudando corpo de jogador e adicionando camera seguidora

---
+++ Projeto1/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour {
+
+	public GameObject Player;
+
+	private Vector3 offset;
+
+	void Start () {
+		offset = Player.transform.position + transform.position;
+	}
+	
+	void LateUpdate () {
+		transform.position = offset + Player.transform.position;
+	}
+}
