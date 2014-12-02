72a7b1bd4a74e43f8b45485d4c54e76c1c8dcf5b
Thiago Jedi <thiagoa7@gmail.com>
2014-12-02 01:02:27 -0300

    Adicionado Projeto 3

---
+++ Projeto3/Assets/Scripts/LifeSpan.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LifeSpan : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void LateUpdate () {
+		if (transform.position.y < 0.0)
+			Destroy(this.gameObject);
+	}
+}
---
+++ Projeto3/Assets/Scripts/Navigate.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Navigate : MonoBehaviour {
+
+	public void Sair() {
+		Application.Quit();
+	}
+}
